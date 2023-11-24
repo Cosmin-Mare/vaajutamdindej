c573d997e43e5f121109acba44d1ffad4d392397
cosmin <cosmintudor.mare12@gmail.com>
2023-11-24 13:51:59 +0200

    Add page Motive and the backbone for donez and ajut. Fixed rendering errors as well as completed the Despre-noi page

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ app.get("/despre-noi", (req, res) => {
   res.render("despre-noi.ejs");
 });
 
+app.get("/motive", (req, res) => {
+  res.render("motive.ejs");
+});
+
+app.get("/donez", (req, res) => {
+  res.render("donez.ejs");
+});
+
+app.get("/cum-pot-ajuta", (req, res) => {
+  res.render("ajut.ejs");
+});
+
 app.listen(process.env.PORT || port, "192.168.1.161", () => {
   console.log("Server open on port " + port);
 });
