19ee059c797c6cad62b07b91e3556069c60b4214
cosmin <cosmintudor.mare12@gmail.com>
2023-12-03 16:00:40 +0200

    finish donation page with all the required links plus recurring and make navbar to appear on scroll

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3000;
-
 const postPhotosPath = "public/images/posts/";
 
 const db = new pg.Client({
@@ -23,17 +22,28 @@ const db = new pg.Client({
 let posts = [];
 
 db.connect();
-db.query("SELECT * FROM posts", (err, res) => {
+let max_id = 0;
+db.query("SELECT * FROM posts ORDER BY id ASC", (err, res) => {
   if (err) {
     console.log(err);
   } else {
     posts = res.rows;
+    posts.reverse();
+    max_id = posts.length;
   }
   db.end();
 });
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 1400;
+};
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -56,20 +66,23 @@ app.get("/cum-pot-ajuta", (req, res) => {
 });
 
 app.get("/post/:id", (req, res) => {
-  const post = getPostById(req.params.id);
+  const post = getPostById(max_id - req.params.id);
   const title = post.title;
   const content = post.content;
   const date = post.date;
   const photos = [];
-  const numberOfPhotos = getNumberOfFilesInFolder(postPhotosPath + post.id);
+  const numberOfPhotos = getNumberOfFilesInFolder(postPhotosPath + post.id) - 1;
   for (let i = 0; i < numberOfPhotos; i++) {
     photos.push("/images/posts/" + post.id + "/" + i + ".jpg");
   }
+  const thumbnail = "/images/posts/" + post.id + "/thumbnail.jpg";
+  const contents = content.split("\n");
   res.render("post.ejs", {
     title: title,
-    content: content,
+    contents: contents,
     date: date,
     photos: photos,
+    thumbnail: thumbnail,
   });
 });
 
@@ -83,7 +96,7 @@ app.listen(process.env.PORT || port, "192.168.1.161", () => {
 
 function getPostById(id) {
   for (let i = 0; i < posts.length; i++) {
-    if (posts[i].id == id) {
+    if (posts[i].id == max_id - id) {
       return posts[i];
     }
   }
